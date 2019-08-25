11daf7afebe79b329d5ee40a9d548ee015653324
Bane Manojlovic <bane1manojlovic@gmail.com>
2019-08-25 17:22:58 +0200

    Initial Commit

---
+++ BITAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BITAlert
+//
+//  Created by Bane Manojlovic on 8/25/19.
+//  Copyright © 2019 Bane Manojlovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
